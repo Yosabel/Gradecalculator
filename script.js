098a40f50c0fa623c01c4cf7e65208c8e906e8e5
yosabel tekeste <yosabeltekeste@students.berkeley.net>
2017-12-01 11:35:50 -0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+
+function calculateCurrentGrade(){
+    // → takes data from page, calls on sub-functions to calculate the student grade and output it back to page.  Also “return” the result so that calculateGradeNeeded() can use it.
+}
+
+function convertArrayStringToNumber(){
+    // takes an array of strings (from page) and returns the same array, except all the items are numbers.
+// Use string.split(“,”)  to convert a string into an array of strings, then iterate through and convert each item in the array into a number like:
+//     array[i] = parseInt(array[i])
+//     averageArray()
+// → takes an array of numbers and returns the average of those numbers
+}
+
+function calculateGradeNeeded(){
+    // → takes the current grade returned by calculateCurrentGrade() and the grade desired and does the math to determine what the user needs on the final.
+}
+

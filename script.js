7a68098704f0d5d7db60e2ef0c892e2e4e43b306
yosabel tekeste <yosabeltekeste@students.berkeley.net>
2017-12-05 11:34:58 -0800

    hey

--- script.js
+++ script.js
@@ -1,15 +1,50 @@
 
 function calculateCurrentGrade(){
+    var homework = document.getElementById("hw").value;
+    var classwork= document.getElementById("cw").value;
+    var tq= document.getElementById("tq").value;
+    var hwAverage = averageArray(convertArrayStringToNumber(homework));
+    var cwAverage=averageArray(convertArrayStringToNumber(classwork));
+    var tqAverage=averageArray(convertArrayStringToNumber(tq));
+return hwAverage + cwAverage+tqAverage/3
+    console.log(homework);
+    // var classworkArray = convertArrayStringToNumber(classwork);
+    // var classworkAvg = averageArray(classworkArray);
+
     // → takes data from page, calls on sub-functions to calculate the student grade and output it back to page.  Also “return” the result so that calculateGradeNeeded() can use it.
 }
 
-function convertArrayStringToNumber(){
+
+
+
+
+
+
+
+
+
+
+function convertArrayStringToNumber(string) {
+    var array = string.split(",");
+    for (var i = 1; i <= array.length; i++) {
+        array[i] = parseInt(array[i]);
+    }
+    return array;
+    
     // takes an array of strings (from page) and returns the same array, except all the items are numbers.
 // Use string.split(“,”)  to convert a string into an array of strings, then iterate through and convert each item in the array into a number like:
 //     array[i] = parseInt(array[i])
-//     averageArray()
-// → takes an array of numbers and returns the average of those numbers
 }
+  function averageArray(array){
+    var sum=0;
+      for(var i=0; i<=array.length;i++){
+    sum+=array[i];
+    console.log(avg)
+      }
+      return sum/array.length;
+      // → takes an array of numbers and returns the average of those numbers
+  }
+
 
 function calculateGradeNeeded(){
     // → takes the current grade returned by calculateCurrentGrade() and the grade desired and does the math to determine what the user needs on the final.

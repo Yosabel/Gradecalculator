cb3fa1abefedd18a8bd26389a0860e48324ee77f
yosabel tekeste <yosabeltekeste@students.berkeley.net>
2017-12-07 11:36:06 -0800

    hey

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 
 function calculateCurrentGrade(){
+    console.log("hi my nae is");
     var homework = document.getElementById("hw").value;
+    console.log(homework);
     var classwork= document.getElementById("cw").value;
-    var tq= document.getElementById("tq").value;
+    var tq = document.getElementById("tq").value;
+    var homeworkWeight=document.getElementById("hwweight").value;
+    var classworkWeight=document.getElementById("cwweight").value;
+    var tqWeight =document.getElementById("tqweight").value;
+    console.log("hi my name is");
     var hwAverage = averageArray(convertArrayStringToNumber(homework));
     var cwAverage=averageArray(convertArrayStringToNumber(classwork));
     var tqAverage=averageArray(convertArrayStringToNumber(tq));
-return hwAverage + cwAverage+tqAverage/3
-    console.log(homework);
+   var finalHw= (hwAverage * (homeworkWeight/100));
+   var finalCw=(cwAverage * (classworkWeight/100));
+   var finalTq=(tqAverage * (tqWeight/100));
+   console.log(finalTq);
+   var finalGrade= ((finalHw+finalCw+finalTq)/(homeworkWeight+classworkWeight+tqWeight));
+    console.log(finalGrade);
+    return finalGrade;
+
     // var classworkArray = convertArrayStringToNumber(classwork);
     // var classworkAvg = averageArray(classworkArray);
 
@@ -26,27 +38,28 @@ return hwAverage + cwAverage+tqAverage/3
 
 function convertArrayStringToNumber(string) {
     var array = string.split(",");
-    for (var i = 1; i <= array.length; i++) {
+    for (var i = 0; i < array.length; i++) {
         array[i] = parseInt(array[i]);
     }
     return array;
-    
+
     // takes an array of strings (from page) and returns the same array, except all the items are numbers.
 // Use string.split(“,”)  to convert a string into an array of strings, then iterate through and convert each item in the array into a number like:
 //     array[i] = parseInt(array[i])
 }
   function averageArray(array){
+    console.log(array);
     var sum=0;
       for(var i=0; i<=array.length;i++){
     sum+=array[i];
-    console.log(avg)
+    console.log(sum)
       }
       return sum/array.length;
       // → takes an array of numbers and returns the average of those numbers
   }
 
 
-function calculateGradeNeeded(){
-    // → takes the current grade returned by calculateCurrentGrade() and the grade desired and does the math to determine what the user needs on the final.
-}
-
+// function calculateGradeNeeded(){
+//     // → takes the current grade returned by calculateCurrentGrade() and the grade desired and does the math to determine what the user needs on the final.
+// }
+//

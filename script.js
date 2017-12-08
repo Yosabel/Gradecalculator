2e2a564f8cb2ace283fedc37a8991a5c752040a8
yosabel tekeste <yosabeltekeste@students.berkeley.net>
2017-12-08 11:35:57 -0800

    hey

--- script.js
+++ script.js
@@ -5,36 +5,34 @@ function calculateCurrentGrade(){
     console.log(homework);
     var classwork= document.getElementById("cw").value;
     var tq = document.getElementById("tq").value;
-    var homeworkWeight=document.getElementById("hwweight").value;
-    var classworkWeight=document.getElementById("cwweight").value;
-    var tqWeight =document.getElementById("tqweight").value;
+    var homeworkWeight=parseInt(document.getElementById("hwweight").value);
+    var classworkWeight=parseInt(document.getElementById("cwweight").value);
+    var tqWeight =parseInt(document.getElementById("tqweight").value);
     console.log("hi my name is");
     var hwAverage = averageArray(convertArrayStringToNumber(homework));
     var cwAverage=averageArray(convertArrayStringToNumber(classwork));
     var tqAverage=averageArray(convertArrayStringToNumber(tq));
+
    var finalHw= (hwAverage * (homeworkWeight/100));
+   console.log(finalHw);
    var finalCw=(cwAverage * (classworkWeight/100));
+    console.log(finalCw);
    var finalTq=(tqAverage * (tqWeight/100));
    console.log(finalTq);
-   var finalGrade= ((finalHw+finalCw+finalTq)/(homeworkWeight+classworkWeight+tqWeight));
+   var finalWeight= homeworkWeight+classworkWeight+tqWeight;
+   console.log(finalWeight);
+   var totalPoints=finalHw+finalCw+finalTq;
+   console.log(totalPoints);
+   var finalGrade= (totalPoints/finalWeight)*100;
     console.log(finalGrade);
-    return finalGrade;
+    document.getElementById("finalGrade").innerHTML=finalGrade;
+    return "you have a" + finalGrade + "%";
 
     // var classworkArray = convertArrayStringToNumber(classwork);
     // var classworkAvg = averageArray(classworkArray);
 
     // → takes data from page, calls on sub-functions to calculate the student grade and output it back to page.  Also “return” the result so that calculateGradeNeeded() can use it.
 }
-
-
-
-
-
-
-
-
-
-
 
 function convertArrayStringToNumber(string) {
     var array = string.split(",");
@@ -47,19 +45,23 @@ function convertArrayStringToNumber(string) {
 // Use string.split(“,”)  to convert a string into an array of strings, then iterate through and convert each item in the array into a number like:
 //     array[i] = parseInt(array[i])
 }
-  function averageArray(array){
-    console.log(array);
-    var sum=0;
-      for(var i=0; i<=array.length;i++){
-    sum+=array[i];
-    console.log(sum)
+  function averageArray(array) {
+      console.log(array);
+      var sum = 0;
+      for (var i = 0; i < array.length; i++) {
+          sum += array[i];
+          console.log(sum);
+          var average = sum / array.length;
+          console.log(average);
       }
-      return sum/array.length;
+      return average;
+
       // → takes an array of numbers and returns the average of those numbers
   }
 
 
-// function calculateGradeNeeded(){
-//     // → takes the current grade returned by calculateCurrentGrade() and the grade desired and does the math to determine what the user needs on the final.
-// }
-//
+function calculateGradeNeeded(calculateCurrentGrade){
+// calculateCurrentGrade
+    // → takes the current grade returned by calculateCurrentGrade() and the grade desired and does the math to determine what the user needs on the final.
+}
+
